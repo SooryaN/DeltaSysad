c952dddd4e143e2ea521bce71efc6295865eb0c5
SooryaN <nayaran.ayroos@gmail.com>
2015-05-20 18:59:39 +0530

    First Task added
    Write a Script to create 100 folders within the given directory.
    The name of each folder should be folder1/ folder2/ and so on till folder100/
    Inside each folderX/ there should be an empty file named folderX.txt (eg. folder1.txt)
    
    It should be such that ONLY THE OWNER of the files and folders should have read-write-executable permissions.

---
+++ Task_01.py
@@ -0,0 +1,14 @@
+import os
+curr=os.getcwd()
+for i in range (1,4):
+	folderName='folder'+str(i)
+	os.mkdir(folderName)
+	cmd1='chmod 0700 '+folderName
+	os.system(cmd1)
+	os.chdir(folderName)
+	filename=folderName+'.txt'
+	file=open(filename,'a')
+	file.close()
+	cmd2='chmod 0700 '+filename
+	os.system(cmd2)
+	os.chdir(curr)
